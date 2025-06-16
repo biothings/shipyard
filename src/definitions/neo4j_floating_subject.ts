d5b6c105e7c1ce64924e66563a4734896137088c
ctrl-schaff <59380780+ctrl-schaff@users.noreply.github.com>
2025-06-16 10:41:52 -0700

    Load testing development (#1)
    * Add load-test definitions in grafana k6
    
    * Update the neo4j test definitions ...
    
    Replace the hardcoded username and password with an
    environment variable. Corrects the query syntax for the MATCH
    cypher query
    
    * Add the elasticsearch load tests
    
    * Add sampling module to simplify testing logic
    
    * Test modularization ...
    
    Adds a configuration file for different environments (for running
    locally and for running on su08). Provides different scenarios
    to combine the basic smoke test with the load testing in one test file
    
    * Expand testcase options and scenarios
    
    * Expand the README documentation
    
    * Update dockerfile and docker-compose along with docs

---
+++ src/definitions/neo4j_floating_subject.ts
@@ -0,0 +1,68 @@
+import encoding from 'k6/encoding';
+import http from 'k6/http';
+import sql from "k6/x/sql";
+
+import driver from "k6/x/sql/driver/sqlite3";
+
+import { neo4j_floating_subject_query } from './graph_sampling.ts';
+import { TestConfiguration } from './configuration.ts';
+
+const graph_db = sql.open(driver, "/src/data/graph_sample.db");
+
+export const options = {
+  scenarios: {
+    smoke: {
+      executor: 'shared-iterations',
+      startTime: '0s',
+      gracefulStop: '15s',
+      env: { NUM_SAMPLE: '1', HTTP_TIMEOUT: '15s' },
+      vus: 1,
+      iterations: 1,
+      maxDuration: '30s',
+    },
+    half_load: {
+      executor: 'shared-iterations',
+      startTime: '30s',
+      gracefulStop: '60s',
+      env: { NUM_SAMPLE: '500', HTTP_TIMEOUT: '1000s' },
+      vus: 15,
+      iterations: 100,
+      maxDuration: '60m',
+    },
+    full_load: {
+      executor: 'shared-iterations',
+      startTime: '60m',
+      gracefulStop: '60s',
+      env: { NUM_SAMPLE: '1000', HTTP_TIMEOUT: '1500s' },
+      vus: 5,
+      iterations: 25,
+      maxDuration: '60m',
+    }
+  },
+};
+
+export function setup() {
+  const USERNAME: string = `${__ENV.NEO4J_USERNAME}`;
+  const PASSWORD: string = `${__ENV.NEO4J_PASSWORD}`;
+  const credentials = encoding.b64encode(`${USERNAME}:${PASSWORD}`);
+
+  const params = {
+    headers: {
+      'Authorization': `Basic ${credentials}`,
+      'Content-Type': 'application/json'
+    },
+    timeout: '60s'
+  };
+  return { params: params }
+}
+
+export function teardown() {
+  graph_db.close();
+}
+
+export default function (data: Object) {
+  const payload: string = neo4j_floating_subject_query(graph_db, 1000);
+  const url: string = TestConfiguration["NEO4J_QUERY_URL"];
+  data.params.timeout = __ENV.HTTP_TIMEOUT;
+  http.post(url, payload, data.params);
+}
