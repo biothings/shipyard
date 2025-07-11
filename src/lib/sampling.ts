8ccc3b56ca922d8b408ec7d641b170448de5d3b6
shuchenliu <shuchenliu2014@gmail.com>
2025-07-10 18:35:13 -0700

    fix table name for graph sample

--- src/lib/sampling.ts
+++ src/lib/sampling.ts
@@ -2,7 +2,7 @@ import sql from "k6/x/sql";
 
 
 export function graph_samples(sampling_database: Database, sample_size: int) {
-  const sample_query: string = "SELECT * FROM graph_samples WHERE rowid IN (SELECT rowid FROM graph_samples ORDER BY random() LIMIT $1)";
+  const sample_query: string = "SELECT * FROM graph_sample WHERE rowid IN (SELECT rowid FROM graph_sample ORDER BY random() LIMIT $1)";
   const samples: Array<{object}> = sampling_database.query(sample_query, sample_size);
   return samples;
 }
