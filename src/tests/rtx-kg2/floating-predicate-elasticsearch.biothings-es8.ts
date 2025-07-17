06261ad09597020f6f566a383e1001be071908ab
shuchenliu <shuchenliu2014@gmail.com>
2025-07-17 07:19:16 +0000

    add floating queries for object and predicate

---
+++ src/tests/rtx-kg2/floating-predicate-elasticsearch.biothings-es8.ts
@@ -0,0 +1,7 @@
+import allModules from "./floating-es-meta/esFloatingMetaQuery.ts";
+
+const {main, handleSummary } = allModules('predicate')
+
+export * from "./floating-es-meta/esFloatingMetaQuery.ts";
+export { handleSummary }
+export default main
