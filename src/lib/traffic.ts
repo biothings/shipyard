8686d888776de7638c6e069b0f35fcda1631dd5a
ctrl-schaff <59380780+ctrl-schaff@users.noreply.github.com>
2025-07-21 09:48:19 -0500

    Cleanup the histogram bin generation (#7)

--- src/lib/traffic.ts
+++ src/lib/traffic.ts
@@ -1,28 +1,36 @@
-/*
- * Generate a statistical distribution based off the traffic * logs provided by nodenorm. 
- * 
- * Takes the number of curies over a one week period and we
- * aggregate all of them and chunk them into 5 minute 
- * sections
- * 
- */
 export function curieTrafficHistogram(curies: number[]): { bins: number[][], probabilities: number[] } {
   const minimumCuries = 1;
   const maximumCuries = 3000;
-  const numberBins = 10;
-  const binWidth = (maximumCuries - minimumCuries) / numberBins;
+  const bins: number[][] = [[1, 10], [10, 300]];
 
-  let count: number = 0;
-  for (const curie of curies) {
-    if (minimumCuries <= curie && curie < binWidth) {
-      count++;
+  // Hard-coding the bucket distributions based off the kind of queries we want based off some
+  // initial analysis of the nodenorm traffic
+  //
+  // Histogram bin 0: Most common bin. Around 50% of the queries should be within this bin of 1-10
+  // curies per query
+  // Histogram bin 1: Second most common bin. Around 90% of the queries were less than 300 curies in
+  // a query, so this bin of 10-300 should be around 30-40%
+  // Histogram bin 2: Least common bin. We're slightly modifying the numbers here given the traffic
+  // test is around an hour and we want about a 10% for a spike of greater than 1000 curies. In the
+  // actual data around 12% of the curies are greater than 300, but we're massaging this for the
+  // test to be more generous to get higher spike based traffic
+
+  const probabilities: number[] = [];
+  let cumulative: number = 0;
+  for (const bin of bins) { 
+    let count: number = 0;
+    for (const curie of curies) {
+      if (bin[0] <= curie && curie < bin[1]) {
+        count++;
+      }
     }
+    probabilities.push(count / curies.length);
+    cumulative += (count / curies.length);
   }
 
-  const probabilities: number[] = [0, 1];
-  // probabilities.push(count / curies.length);
-  // probabilities.push(1 - probabilities[0]);
-  const bins: number[][] = [[minimumCuries, minimumCuries + binWidth], [maximumCuries / 3, maximumCuries]];
+  probabilities.push(1 - cumulative)
+  bins.push([maximumCuries / 3, maximumCuries])
+
   return { bins, probabilities };
 }
 
