266d895226da0fe46f6af5367b6e17ed04d52e86
ctrl-schaff <schaffjohnathan@gmail.com>
2025-08-12 14:25:58 -0500

    Adjust janusgraph load

--- src/tests/rtx-kg2/fixed.janusgraph.su08.ts
+++ src/tests/rtx-kg2/fixed.janusgraph.su08.ts
@@ -15,7 +15,7 @@ export const options = {
       executor: 'shared-iterations',
       startTime: '0m',
       gracefulStop: '30s',
-      env: { NUM_SAMPLE: '1000', HTTP_TIMEOUT: '300s'},
+      env: { NUM_SAMPLE: '200', HTTP_TIMEOUT: '300s'},
       vus: 5,
       iterations: 25,
       maxDuration: '10m',
