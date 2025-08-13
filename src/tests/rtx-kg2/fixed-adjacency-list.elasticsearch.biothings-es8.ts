e21d94252a2ffb414812942e6a030efb82e6d6c2
shuchenliu <shuchenliu2014@gmail.com>
2025-08-12 21:33:10 -0700

    fix index name error

--- src/tests/rtx-kg2/fixed-adjacency-list.elasticsearch.biothings-es8.ts
+++ src/tests/rtx-kg2/fixed-adjacency-list.elasticsearch.biothings-es8.ts
@@ -1,4 +1,4 @@
-import { es_fixed_query } from "../../lib/graph.ts";
+import { es_fixed_query, IndexName } from "../../lib/graph.ts";
 import { EnvConfiguration } from "../../configuration/environment.ts";
 import http from "k6/http";
 
@@ -10,7 +10,7 @@ import { graph_db } from "../../configuration/db.ts";
 import { LoadPayload } from "../../typings/shipyard/load.ts";
 
 export default function (data: LoadPayload) {
-  const index: string = "rtx_kg2_edges";
+  const index: IndexName = "rtx_kg2_nodes_adjacency_list";
   const payload: string = es_fixed_query(graph_db, __ENV.NUM_SAMPLE, index);
   const url: string = EnvConfiguration["ES_QUERY_URL"]["su12"];
   data.params.timeout = __ENV.HTTP_TIMEOUT;
