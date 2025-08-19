c8704cbbdba930cd5962618d70aeea9782d99b90
Everaldo <everaldorodrigo@gmail.com>
2025-08-19 11:33:24 -0700

    Add dgraph queries.

---
+++ src/tests/rtx-kg2/floating-predicate.dgraph.su08.ts
@@ -0,0 +1,51 @@
+import encoding from "k6/encoding";
+import http from "k6/http";
+import sql from "k6/x/sql";
+
+import driver from "k6/x/sql/driver/sqlite3";
+
+import { dgraphFloatingPredicateQuery } from "../../lib/graph";
+import { EnvConfiguration } from "../../configuration/environment";
+
+const graphDB = sql.open(driver, "/src/data/graph_sample.db");
+
+export const options = {
+  scenarios: {
+    full_load: {
+      executor: "shared-iterations",
+      startTime: "0s",
+      gracefulStop: "60s",
+      env: { NUM_SAMPLE: "1000", HTTP_TIMEOUT: "1500s" },
+      vus: 5,
+      iterations: 25,
+      maxDuration: "60m",
+    },
+  },
+};
+
+export function setup() {
+  const params = {
+    headers: {
+      "Content-Type": "application/dql",
+    },
+    timeout: "60s",
+  };
+  return { params: params };
+}
+
+export function teardown() {
+  graphDB.close();
+}
+
+export default function (data: Object) {
+  const payload: string = dgraphFloatingPredicateQuery(graphDB, __ENV.NUM_SAMPLE);
+  const url: string = EnvConfiguration["DGRAPH_QUERY_URL"];
+  data.params.timeout = __ENV.HTTP_TIMEOUT;
+  http.post(url, payload, data.params);
+}
+
+export function handleSummary(data) {
+  return {
+    "/testoutput/floating-predicate.dgraph.su08.ts.json": JSON.stringify(data),
+  };
+}
