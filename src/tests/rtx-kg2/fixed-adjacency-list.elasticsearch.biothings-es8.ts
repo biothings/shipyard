133ccfae1281c4e7f71be000aaad959d35a7e74d
ctrl-schaff <schaffjohnathan@gmail.com>
2025-08-26 22:27:18 -0500

    Correct older function calls to newer format

--- src/tests/rtx-kg2/fixed-adjacency-list.elasticsearch.biothings-es8.ts
+++ src/tests/rtx-kg2/fixed-adjacency-list.elasticsearch.biothings-es8.ts
@@ -1,4 +1,4 @@
-import { es_fixed_query, IndexName } from "../../lib/graph.ts";
+import { esFixedQuery, IndexName } from "../../lib/graph.ts";
 import { EnvConfiguration } from "../../configuration/environment.ts";
 import http from "k6/http";
 
@@ -11,7 +11,7 @@ import { LoadPayload } from "../../typings/shipyard/load.ts";
 
 export default function (data: LoadPayload) {
   const index: IndexName = "rtx_kg2_nodes_adjacency_list";
-  const payload: string = es_fixed_query(graph_db, __ENV.NUM_SAMPLE, index);
+  const payload: string = esFixedQuery(graph_db, __ENV.NUM_SAMPLE, index);
   const url: string = EnvConfiguration["ES_QUERY_URL"]["su12"];
   data.params.timeout = __ENV.HTTP_TIMEOUT;
   http.post(url, payload, data.params);
