655f2c6551509c1b9761fa7f548857778bd8a669
ctrl-schaff <schaffjohnathan@gmail.com>
2025-09-02 19:32:12 -0500

    Add renci-ci environment tests

---
+++ src/tests/nodenorm/stress.redis.renci.ci.ts
@@ -0,0 +1,52 @@
+import http from "k6/http";
+import sql from "k6/x/sql";
+
+import driver from "k6/x/sql/driver/sqlite3";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.1.0/index.js";
+
+import { nodenormRedisQuery } from "../../lib/curie.ts";
+import { EnvConfiguration } from "../../configuration/environment.ts";
+
+const curie_db = sql.open(driver, "/src/data/nodenorm_curie.db");
+
+export const options = {
+  scenarios: {
+    full_load: {
+      executor: "shared-iterations",
+      startTime: "0",
+      gracefulStop: "5s",
+      env: { NUM_SAMPLE: "1000", HTTP_TIMEOUT: "60s" },
+      vus: 5,
+      iterations: 250,
+      maxDuration: "15m",
+    },
+  },
+};
+
+export function setup() {
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    timeout: "60s",
+  };
+  return { params: params };
+}
+
+export function teardown() {
+  curie_db.close();
+}
+
+export default function (data: Object) {
+  const url: string = EnvConfiguration["NODENORM_QUERY_URL"]["renci_ctrl"];
+  const payload: string = nodenormRedisQuery(curie_db, __ENV.NUM_SAMPLE);
+  data.params.timeout = __ENV.HTTP_TIMEOUT;
+  http.post(url, payload, data.params);
+}
+
+export function handleSummary(data) {
+  return {
+    "/testoutput/stress.redis.renci.ci.ts.json": JSON.stringify(data),
+    stdout: textSummary(data, { indent: "→", enableColors: true }),
+  };
+}
---
+++ src/tests/nodenorm/traffic.redis.renci.ci.ts
@@ -0,0 +1,69 @@
+import exec from "k6/execution";
+import http from "k6/http";
+import sql from "k6/x/sql";
+
+import driver from "k6/x/sql/driver/sqlite3";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.1.0/index.js";
+
+import { EnvConfiguration } from "../../configuration/environment.ts";
+import { nodenormRedisQuery } from "../../lib/curie.ts";
+import { sampleCurieTrafficValue } from "../../lib/traffic.ts";
+import { trafficCurieSizes } from "../../lib/sampling.ts";
+
+const curie_db = sql.open(driver, "/src/data/nodenorm_curie.db");
+const traffic_db = sql.open(driver, "/src/data/traffic.db");
+
+export const options = {
+  scenarios: {
+    traffic_simulation: {
+      executor: "constant-vus",
+      vus: 1,
+      env: { NUM_SAMPLE: "10", HTTP_TIMEOUT: "300s" },
+      startTime: "0s",
+      duration: "60m",
+    },
+  },
+};
+
+export function setup() {
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    chunkInterval: [0.0, 600000.0],
+    timeout: "300s",
+  };
+  return { params: params };
+}
+
+export function teardown() {
+  curie_db.close();
+}
+
+export default function (data) {
+  if (exec.instance.currentTestRunDuration > data.params.chunkInterval[1]) {
+    const curieSizeSampling = trafficCurieSizes(traffic_db, -1);
+    let trafficValue = sampleCurieTrafficValue(curieSizeSampling);
+    if (trafficValue > 1000) {
+      trafficValue = 1000;
+    }
+    __ENV.NUM_SAMPLE = trafficValue;
+    console.log("Updated curie traffic throughput to ", trafficValue);
+    const intervalDifference: number =
+      data.params.chunkInterval[1] - data.params.chunkInterval[0];
+    data.params.chunkInterval[0] += intervalDifference;
+    data.params.chunkInterval[1] += intervalDifference;
+  }
+
+  const url: string = EnvConfiguration["NODENORM_QUERY_URL"]["renci_ctrl"];
+  const payload: string = nodenormRedisQuery(curie_db, __ENV.NUM_SAMPLE);
+  data.params.timeout = __ENV.HTTP_TIMEOUT;
+  http.post(url, payload, data.params);
+}
+
+export function handleSummary(data) {
+  return {
+    "/testoutput/traffic.redis.renci.ci.ts.json": JSON.stringify(data),
+    stdout: textSummary(data, { indent: "→", enableColors: true }),
+  };
+}
