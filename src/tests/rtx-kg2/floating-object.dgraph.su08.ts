4feabc51d60d71cc7ace02e875179f0ede2dbc48
ctrl-schaff <schaffjohnathan@gmail.com>
2025-09-05 11:14:43 -0500

    Remove the unused HTTP response size trend

--- src/tests/rtx-kg2/floating-object.dgraph.su08.ts
+++ src/tests/rtx-kg2/floating-object.dgraph.su08.ts
@@ -1,7 +1,6 @@
 import http from "k6/http";
 import sql from "k6/x/sql";
 
-import { Trend } from "k6/metrics";
 import driver from "k6/x/sql/driver/sqlite3";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.1.0/index.js";
 
@@ -9,8 +8,6 @@ import { dgraphFloatingObjectQuery } from "../../lib/graph.ts";
 import { EnvConfiguration } from "../../configuration/environment.ts";
 
 const graphDB = sql.open(driver, "/src/data/graph_sample.db");
-
-const respSizeTrend = new Trend("http_resp");
 
 export const options = {
   scenarios: {
@@ -44,8 +41,7 @@ export default function (data: Object) {
   const payload: string = dgraphFloatingObjectQuery(graphDB, __ENV.NUM_SAMPLE);
   const url: string = EnvConfiguration["DGRAPH_QUERY_URL"];
   data.params.timeout = __ENV.HTTP_TIMEOUT;
-  const resp: http.Response = http.post(url, payload, data.params);
-  respSizeTrend.add(resp.body.length);
+  http.post(url, payload, data.params);
 }
 
 export function handleSummary(data) {
